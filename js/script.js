6266e97cb4a83700b18e8bddb52dcfa0666b9c69
yurik-007 <yurik-007@mail.ru>
2022-02-07 16:23:15 +0300

    041_практика_2_timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', (e) => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -42,4 +44,57 @@ window.addEventListener('DOMContentLoaded', (e) => {
         }
     });
 
+    //Timer
+
+    const deadline = '2022-02-17';
+    // const z = new Date(deadline);
+    // console.log(typeof(z));
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    function getZiro(num){
+        if(num >=0 && num < 10){
+            return `0${num}`;
+        }
+        else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+
+            timeInterval= setInterval(updateClock,1000);
+
+            updateClock();
+
+            function updateClock(){
+                const t=getTimeRemaining(endtime);
+
+                days.innerHTML=getZiro(t.days);
+                hours.innerHTML=getZiro(t.hours);
+                minutes.innerHTML=getZiro(t.minutes);
+                seconds.innerHTML=getZiro(t.seconds);
+
+            }
+
+    }
+setClock('.timer', deadline);
 });
