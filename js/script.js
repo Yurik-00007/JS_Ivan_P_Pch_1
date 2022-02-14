e314ed7e5dd796f9889cec0ed307d793aaae0ae5
yurik-007 <yurik-007@mail.ru>
2022-02-14 17:27:14 +0300

    048

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
         }
     });
 
-    const modelTimerId = setInterval(openModal, 15000);
+     const modelTimerId = setInterval(openModal, 15000);
 
     function showModalByScroll() {
         if ((window.pageYOffset + document.documentElement.clientHeight + 1) >= document.documentElement.scrollHeight) {
@@ -152,5 +152,73 @@ window.addEventListener('DOMContentLoaded', (e) => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+    // Используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent=document.querySelector(parentSelector);//указываем родителя, куда будут помещаться эти карточки
+            this.transfer = 27;//фиксированый курс 
+            this.changeToUAH();
+            // вызываем метод который изменяет валюту и возвращает значение this.price
+
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            
+            element.innerHTML = //делаем обект универсальным, вставляе атрибуты
+            `<div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+            `;
+            this.parent.append(element);
+        }    
+
+    }
+const div1 = new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    9,
+    '.menu .container'
+    );
+    div1.render();
+
+    const div2 = new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container'
+        );
+        div2.render();
+
+        const div3 = new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие           продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное           количество белков за счет тофу и импортных вегетарианских стейков.',
+            21,
+            '.menu .container'
+            );
+            div3.render();
 
 });
