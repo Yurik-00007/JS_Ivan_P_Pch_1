6df4a9a1ff70413e0d43e782893175d831bece46
yurik-007 <yurik-007@mail.ru>
2022-02-26 18:17:01 +0300

    059_получение_данных с сервера

--- js/script.js
+++ js/script.js
@@ -216,38 +216,81 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     }
-    const div1 = new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        9,
-        '.menu .container',
-        'menu__item',
-        'big'
-    );
-    div1.render();
 
-    const div2 = new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container'
-    );
-    div2.render();
+    const getResource = async(url) =>{
+        const res = await fetch(url);
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    const div3 = new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие           продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное           количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container'
+        return await res.json();
+    };
 
-    );
-    div3.render();
+    getResource('http://localhost:3000/menu',)
+    .then(data =>{
+        data.forEach(({img, altimg, title, descr, price}) =>{
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+    });
+
+    // const div1 = new MenuCard(
+    //     "img/tabs/vegy.jpg",
+    //     "vegy",
+    //     'Меню "Фитнес"',
+    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    //     9,
+    //     '.menu .container',
+    //     'menu__item',
+    //     'big'
+    // );
+    // div1.render();
+
+    // const div2 = new MenuCard(
+    //     "img/tabs/elite.jpg",
+    //     "elite",
+    //     'Меню “Премиум”',
+    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    //     14,
+    //     '.menu .container'
+    // );
+    // div2.render();
+
+    // const div3 = new MenuCard(
+    //     "img/tabs/post.jpg",
+    //     "post",
+    //     'Меню "Постное"',
+    //     'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие           продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное           количество белков за счет тофу и импортных вегетарианских стейков.',
+    //     21,
+    //     '.menu .container'
+
+    // );
+    // div3.render();
+
+    // getResource('http://localhost:3000/menu',)
+    // .then(data => createCard(data));
+
+    //     function createCard(data){
+    //         data.forEach(({img, altimg, title, descr, price}) =>{
+    //             const element = document.createElement('div');
+    //             price*=27;
+
+    //             element.classList.add('menu__item');
+
+    //             element.innerHTML= `
+    //                 <img src=${img} alt=${altimg}>
+    //                 <h3 class="menu__item-subtitle">${title}</h3>
+    //                 <div class="menu__item-descr">${descr}</div>
+    //                 <div class="menu__item-divider"></div>
+    //                 <div class="menu__item-price">
+    //                     <div class="menu__item-cost">Цена:</div>
+    //                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //                 </div>
+    //             `; 
+
+    //             document.querySelector('.menu .container').append(element);
+
+    //         });
+    //     }
 
     //Forms
 
@@ -260,10 +303,21 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async(url,data) =>{
+        const res = await fetch(url,{
+            method : 'POST',
+            headers : {
+                'Content-type':'application/json'
+            },
+            body : data
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -282,23 +336,24 @@ window.addEventListener('DOMContentLoaded', () => {
             
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            // const object = {};
+            // formData.forEach(function (value, key) {
+            //     object[key] = value;
+            // });
 
-            // const json = JSON.stringify(object);
+             const json = JSON.stringify(Object.fromEntries(formData.entries()));
+             console.log(json);
+
+             const obj ={a:23, b:50};
+             const w=Object.entries(obj);
+             console.log(Object.fromEntries(w));
 
 
-            fetch('server.php',{
-                method : 'POST',
-                headers : {
-                    'Content-type':'application/json'
-                },
-                body : JSON.stringify(object)
-            }).then(data => data.text())
+
+      
+            postData('http://localhost:3000/requests', json)
             .then(data =>{
-                console.log(data);
+                 console.log(data);
                 showThanksModal(message.success);
                 statusMessage.remove();
             }).catch(()=>{
@@ -361,7 +416,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //     // console.log(request1.open('GET','https://jsonplaceholder.typicode.com/todos/1'));
 
     fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res)); 
+        .then(data => data.json());
+        // .then(res => console.log(res)); 
 
 });
