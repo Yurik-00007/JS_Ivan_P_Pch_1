f94cd75f6ecbf782cbcc8b4f071ff838c0e0bbd3
yurik-007 <yurik-007@mail.ru>
2022-03-04 18:01:33 +0300

    dots

--- js/script.js
+++ js/script.js
@@ -439,6 +439,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //Slider
 
     const sliders = document.querySelectorAll('.offer__slide'),
+        slider=document.querySelector('.offer__slider'),//обратиться и установить позицию relativ
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total =document.querySelector('#total'),
@@ -448,6 +449,16 @@ window.addEventListener('DOMContentLoaded', () => {
         width = window.getComputedStyle(slidesWrapper).width;
     let slideIndex = 1,
         offset=0;// отступ
+
+        function currVal(slideIndex){
+            if(sliders.length<10){
+                current.textContent=`0${slideIndex}`;
+            }else{
+                current.textContent=slideIndex;
+            }
+        }
+
+        
 
         if(sliders.length<10){
             total.textContent=`0${sliders.length}`;
@@ -471,6 +482,31 @@ window.addEventListener('DOMContentLoaded', () => {
         
     });
 
+    slider.style.position = 'relative';// теперь все элементы КОТОРЫЕ  будут абсолютно спазиционированы внутри слайдера будут нормально отображаться
+    // создать большую обертку со всех точек и както ее застилизовать 
+    const indicators = document.createElement('ol'),//не div а order list
+        dots=[]; 
+
+    function dotsVal(){
+        dots.forEach(dot => dot.style.opacity='.5');
+        dots[slideIndex - 1].style.opacity=1;
+    }
+
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for(let i=0; i<sliders.length; i++){
+        const dot = document.createElement('li');// list item
+        dot.setAttribute('data-slide-to', i+1);
+        dot.classList.add('dot');
+        if(i==0){
+            dot.style.opacity=1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+    
+
     next.addEventListener('click', ()=>{
         if(offset ==+width.slice(0, width.length-2)*(sliders.length-1)){
             offset=0;
@@ -487,11 +523,9 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if(sliders.length<10){
-            current.textContent=`0${slideIndex}`;
-        }else{
-            current.textContent=slideIndex;
-        }
+        currVal(slideIndex);
+
+        dotsVal();
     });
 
     prev.addEventListener('click', ()=>{
@@ -511,21 +545,37 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        if(sliders.length<10){
-            current.textContent=`0${slideIndex}`;
-        }else{
-            current.textContent=slideIndex;
-        }
+        currVal(slideIndex);
+
+        dotsVal();
+
+    });
+
+    dots.forEach(dot =>{
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset= +width.slice(0, width.length-2)*(slideTo-1);
+
+            slidesField.style.transform=`translateX(-${offset}px)`;
+
+        currVal(slideIndex);
+
+        dotsVal();
+
+            
+        });
     });
 
 
     // showSlides(slideIndex);
 
-    if(sliders.length<10){
-        total.textContent=`0${sliders.length}`;
-    }else{
-        total.textContent=`${sliders.length}`;
-    }
+    // if(sliders.length<10){
+    //     total.textContent=`0${sliders.length}`;
+    // }else{
+    //     total.textContent=`${sliders.length}`;
+    // }
     
 
     // function showSlides(n) {
